890b3439f95e6599444adc22b9d869a23a8de5b7
yotamaeda <72425430+yotamaeda@users.noreply.github.com>
2020-10-26 23:16:25 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,9 +3,37 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+  for(let i = 0; i <= 8; i++){
+    for(let j = 0; j <= 8; j++){
+    if(i%2==0&&j%2==0){
+      fill(255);
+    }
+    if(i%2==1&&j%2==1){
+      fill(255);
+    }
+    else if(i==j){
+      fill(255);
+    }
+    else if(i%2==0&&j==0){
+      fill(255);
+    }
+    else if(i%2==0&&j==2){
+      fill(255);
+    }
+    else if(i%2==0&&j==4){
+      fill(255);
+    }
+    else if (i%2==0&&j==6) {
+      fill(255);
+    }
+    else if(i%2==0&&j==8){
+      fill(255);
+    }
+    else{
+      fill(143);
+    } // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    rect(size*i, size*j, size, size)
+    if()
     }
   }
 }
